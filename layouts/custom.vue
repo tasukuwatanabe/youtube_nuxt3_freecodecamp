e04f288ea40a863f607191475bdc6fa53ce6dbc8
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-22 00:06:34 +0900

    Layouts

---
+++ layouts/custom.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="CustomPage text-white h-screen bg-slate-900">
+    <p>Custom Layout</p>
+    <slot />
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="h-screen DefaultLayout text-white bg-blue-900">
+    <p>Default Layout</p>
+    <slot />
+  </div>
+</template>
