a5b133a16966526178f78e4c32043de7a5324b6b
mf-watanabe-tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-25 14:11:20 +0900

    State Management

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,18 @@
-<script setup>
-definePageMeta({
-})
+<script setup lang="ts">
+const counter = useCounter();
 </script>
 
 <template>
-  <div class="main">
-    <ContentDoc />
+  <div>
+    <div id="main">
+      Counter: {{ counter }}
+      <button @click="counter++">
+        +
+      </button>
+      <button @click="counter--">
+        -
+      </button>
+    </div>
+    <Counter id="counter" />
   </div>
 </template>
